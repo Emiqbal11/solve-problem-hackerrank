34baaebaddd006401d6db9924d64263aeb45e528
Iqbal Hassan <107492954+Emiqbal11@users.noreply.github.com>
2023-02-17 00:30:02 +0500

    Calculate the perimeter of a rectangle

---
+++ ArithmeticOperators.js
@@ -0,0 +1,14 @@
+function getArea(length, width) {
+    let area;
+    area =3*4.5;
+    // Write your code here
+    
+    return area;
+}
+function getPerimeter(length, width) {
+    let perimeter;
+    // Write your code here
+    perimeter=2*(3+4.5)
+    
+    return perimeter;
+}
