1f3b3b85c6a40ff58f3c37cca06d308d665dbfbe
Iqbal Hassan <107492954+Emiqbal11@users.noreply.github.com>
2023-02-17 00:17:05 +0500

    first program added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function greeting(parameterVariable) {
     // This line prints 'Hello, World!' to the console:
     console.log('Hello, World!');
-    console.log(parameterVariable)
+    console.log(parameterVariabl)
 
     // Write a line of code that prints parameterVariable to stdout using console.log:   
 }
